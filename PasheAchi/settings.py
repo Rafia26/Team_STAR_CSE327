2b322e41b4d413f26381bedd52c2d8e23878bd41
Shahriar Ferdous Shovon <shahriar.shovon@northsouth.edu>
2021-05-09 15:19:11 +0600

    All Function

--- PasheAchi/settings.py
+++ PasheAchi/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'LogInRegister',
+    'viewProfile',
+    'makeAppointment',
+    'viewAppointment',
+    'search',
+    'donate',
+    'addRatings',
+    'viewRatings',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'teamstarcse327@gmail.com'
+EMAIL_HOST_PASSWORD = 'aftarash18173'
