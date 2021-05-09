845b0023ce94f07d602ca1ba8fb55025a1a04bbc
Afsana Binte Rakib <afsana.rakib@northsouth.edu>
2021-05-09 15:22:28 +0600

    Edited the project folder for ease of merging.

--- Project/PasheAchi/PasheAchi/urls.py
+++ Project/PasheAchi/PasheAchi/urls.py
@@ -15,25 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from LogInRegister import views
+
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('', views.login, name="login"),
-    path('contact_us/', views.contact_us, name="contact_us"),
-    path('description/', views.description, name="description"),
-    path('about_us/', views.about_us, name="about_us"),
-    path('google_login/', views.google_login, name="google_login"),
-    path('home/', views.home, name="home"),
-    path('patient_signup/', views.patient_signup, name="patient_signup"),
-    path('doctor_signup/', views.doctor_signup, name="doctor_signup"),
-    path('patient_post_signup/', views.patient_post_signup, name="patient_post_signup"),
-    path('doctor_post_signup/', views.doctor_post_signup, name="doctor_post_signup"),
-    path('logout/', views.logout, name="logout"), 
-    path('forget_password/', views.forget_password, name = "forget_password"),   
-    path('makeAp/', include('makeAppointment.urls')),
-    path('viewAp/', include('viewAppointment.urls')),
     path('search/', include('search.urls')),
     path('donate/', include('donate.urls')),
     
